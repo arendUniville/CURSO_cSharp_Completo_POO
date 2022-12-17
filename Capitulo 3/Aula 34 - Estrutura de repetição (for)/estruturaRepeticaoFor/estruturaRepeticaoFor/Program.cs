db54112a3aab4d24d9a817e3e8fe15496b24bc0c
Arend <matheus.arend@univille.br>
2022-12-17 14:26:48 -0300

    Aula 34 - Step 1

---
+++ Capitulo 3/Aula 34 - Estrutura de repetição (for)/estruturaRepeticaoFor/estruturaRepeticaoFor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace estruturaRepeticaoFor
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantos números INTEIROS você deseja digitar? ");
+
+            int quantNumeros = int.Parse(Console.ReadLine());
+
+
+            int soma = 0;
+
+            for(int i = 1; i <= quantNumeros; i++)
+            {
+                
+                Console.Write("Valor: #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+
+                soma += valor;
+
+            }
+
+            Console.WriteLine("Soma é = "+soma);
+
+        }
+    }
+
+}
