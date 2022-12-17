65376d4232baeea6b06cedb50ffae1e6f5f95a26
Arend <matheus.arend@univille.br>
2022-12-17 13:45:10 -0300

    Aula 30 - Step 1

--- Capitulo 3/Aula 30 - Funções (Sintaxe)/funcoes/funcoes/Program.cs
+++ Capitulo 3/Aula 30 - Funções (Sintaxe)/funcoes/funcoes/Program.cs
@@ -9,8 +9,46 @@ namespace funcoes
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Digite três números: ");
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+
+
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("O maior número é: " + resultado);
 
 
         }
+
+        static int Maior(int a, int b, int c) {
+
+            double maior;
+
+            if (a > b && a > c)
+            {
+
+                Console.WriteLine("O maior número é: " + a);
+
+            }
+            else if (b > c)
+            {
+
+                Console.WriteLine("O maior número é: " + b);
+
+            }
+            else
+            {
+
+                Console.WriteLine("O maior número é: " + c);
+
+            }
+
+        }
     }
+
+
 }
