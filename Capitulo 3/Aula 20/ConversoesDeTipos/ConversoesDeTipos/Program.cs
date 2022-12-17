28f4db72752e447063cb91a4cd1e0421aa7da3d9
Arend <matheus.arend@univille.br>
2022-12-17 09:58:26 -0300

    Aula 20 - Step 1

---
+++ Capitulo 3/Aula 20/ConversoesDeTipos/ConversoesDeTipos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace ConversoesDeTipos { 
+
+    class Program
+    {
+
+        static void Main(string[] args) {
+
+
+        // Double para float.
+        
+            double a;
+            float b;
+
+            a = 5.1;
+            // Método casting, permitindo que haja perda de informação para que o valor seja inserido na variável de um tamanho menor.
+            b = (float)a;
+
+            Console.WriteLine("Resultado A: "+b);
+
+
+        // Double para int.
+
+            double aA = 5.1;
+            int bB = (int)aA;
+
+            Console.WriteLine("Resultado B: "+bB);
+
+
+        // Dividindo dois int e armazenando em double.
+
+            int theA = 5;
+            int theB = 2;
+
+            // Aplico o casting no dividendo, pois é o que importa para o resultado ser correto.
+            double resultado = (double)theA / theB;
+
+            Console.WriteLine("Resultado C: "+resultado);
+
+        }
+    }
+
+
+}
