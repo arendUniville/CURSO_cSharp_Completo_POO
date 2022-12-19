1c440efdc6fbea6b7b56401e9eb857042a0838a9
Arend <matheus.arend@univille.br>
2022-12-19 19:16:56 -0300

    Aula 32 - Step1

---
+++ Capitulo 4/Aula 39 - Criando uma classe com tres atributos/primeiroContatoComPOO/primeiroContatoComPOO/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace primeiroContatoComPOO
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Triangulo x, y;
+            string maiorArea;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Digite as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Digite as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+
+            if(areaX > areaY)
+            {
+
+                maiorArea = "X";
+
+            }
+            else
+            {
+
+                maiorArea = "Y";
+
+            }
+
+            Console.WriteLine("A maior área é: " + maiorArea);
+
+
+
+        }
+    }
+}
