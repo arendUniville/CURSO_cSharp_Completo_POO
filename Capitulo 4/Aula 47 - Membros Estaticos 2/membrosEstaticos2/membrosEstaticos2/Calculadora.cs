ddd4b9548b0312c2cb4811f74d44da66b194d548
Arend <matheus.arend@univille.br>
2022-12-20 19:35:55 -0300

    Aula 47 - Step 1

---
+++ Capitulo 4/Aula 47 - Membros Estaticos 2/membrosEstaticos2/membrosEstaticos2/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace membrosEstaticos2
+{
+    class Calculadora
+    {
+
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r)
+        {
+
+            return 2.0 * Pi * r;
+
+        }
+
+
+        public double Volume(double r)
+        {
+            /* Ao cubo */
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+
+        }
+
+    }
+}
