10fbac31d95f8fe8e658beb15d25de7f4a4ff6a2
Arend <matheus.arend@univille.br>
2022-12-25 12:12:27 -0300

    Aula 56 - Init

---
+++ Capitulo 5/Aula 56 - Properties/aula56_properties/aula56_properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(p.GetNome());
+
+            Console.Write("Digite o nome a ser alterado: ");
+
+            string novoNome = Console.ReadLine();
+
+            p.SetNome(novoNome);
+
+            Console.WriteLine(p.GetNome());
+            
+        }
+    }
+}
