b8604037f7d1ab447c2f51150bf5d7a367ba2bc2
Arend <matheus.arend@univille.br>
2023-01-08 14:22:49 -0300

    aula 72 - Step 2

---
+++ Capitulo 6/Aula 72 - Modificador de parametro params/aula72/aula72/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula72
+{
+    class Calculator
+    {
+
+        public static int Sum(int[] numbers)
+        {
+
+            int sum = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+
+            }
+            return sum;
+
+        }
+
+    }
+}
--- Capitulo 6/Aula 72 - Modificador de parametro params/aula72/aula72/Program.cs
+++ Capitulo 6/Aula 72 - Modificador de parametro params/aula72/aula72/Program.cs
@@ -9,8 +9,12 @@ namespace aula72
 
         static void Main(string[] args) 
         {
-        
-            
+
+            int s1 = Calculator.Sum(new int[] {2, 3});
+            int s2 = Calculator.Sum(new int[] {2, 4, 3});
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
 
         }
 
