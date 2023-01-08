996dc3f9e0540449391c047b0471c0ad6b19a919
Arend <matheus.arend@univille.br>
2023-01-08 13:37:04 -0300

    aula 69 - Step 1

---
+++ Capitulo 6/Aula 69 - Vetores pt1/aula69/aula69/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aula69
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                vect[i] = double.Parse(Console.ReadLine());
+
+            }
+ 
+        }
+    }
+
+}
