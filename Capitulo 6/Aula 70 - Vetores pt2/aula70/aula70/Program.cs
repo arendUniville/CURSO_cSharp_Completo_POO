976e39b3c1f84e2a3cf8ccf2d818d212e441b7a4
Arend <matheus.arend@univille.br>
2023-01-08 14:09:58 -0300

    Aula 70 - Step 1

---
+++ Capitulo 6/Aula 70 - Vetores pt2/aula70/aula70/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace aula70
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Product { Name = name, Price = price };
+
+            }
+
+            double soma = 0.0;
+
+            for(int i = 0; i < n; i++)
+            {
+                soma += vect[i].Price;
+
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("Avarega Price: "+ avg.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
