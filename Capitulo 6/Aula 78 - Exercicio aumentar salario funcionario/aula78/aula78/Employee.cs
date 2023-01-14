7f8ff18798a824e6fbdbc29a4011cf4f08a37a81
Arend <matheus.arend@univille.br>
2023-01-14 12:15:13 -0300

    Aula 78 (Exercicio) - Step 1

---
+++ Capitulo 6/Aula 78 - Exercicio aumentar salario funcionario/aula78/aula78/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace aula78
+{
+    class Employee
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+
+        public Employee() { }
+
+        public Employee(int id, string name, double salary)
+        {
+
+            Id = id;
+            Name = name;
+            Salary = salary;
+
+        }
+
+
+        public void increaseSalary(double increase)
+        {
+
+            double addMarge = Salary * increase;
+
+            Salary += addMarge;
+
+        }
+
+
+        public override string ToString()
+        {
+            return "Id: " + Id + "\r\nNome: " + Name + "\r\nSalário: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Capitulo 6/Aula 78 - Exercicio aumentar salario funcionario/aula78/aula78/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace aula78
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int quantidadeEmpregados;
+
+            Console.Write("Digite quantos empregados você deseja registrar: ");
+            quantidadeEmpregados = int.Parse(Console.ReadLine());
+
+
+            List<Employee> listEmployees = new List<Employee>();
+
+
+            for(int i = 1; i <= quantidadeEmpregados; i++)
+            {
+
+                Employee newEmployee = new Employee();
+
+                Console.WriteLine("\r\nEmpregado N° " + i);
+                
+                Console.Write("Digite o ID: ");
+                newEmployee.Id = int.Parse(Console.ReadLine());
+
+
+                Console.Write("Digite o nome: ");
+                newEmployee.Name = Console.ReadLine();
+
+
+                Console.Write("Digite o salário: ");
+                newEmployee.Salary = double.Parse(Console.ReadLine());
+
+                listEmployees.Add(newEmployee);
+
+            }
+
+
+            Console.WriteLine("\r\nEmpregados(" + listEmployees.Count + "): ");
+            
+
+            foreach (Employee emp in listEmployees)
+            {
+                Console.WriteLine(emp + "\r\n");
+            }
+
+            
+
+        }
+    }
+}
