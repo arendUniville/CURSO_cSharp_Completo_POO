d9182c48bc1542cf319d19e7848c3e4a91cf9192
Arend <matheus.arend@univille.br>
2023-01-15 12:10:12 -0300

    Aula 89 - Step 1

---
+++ Capitulo 7/Aula 89 - DateTime/aula89/aula89/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula89
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            DateTime d1 = DateTime.Now;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+
+            long numeroMaisAleatorioPossivel = (d1.Ticks * 3) / 233333333666999;
+
+            Console.WriteLine(numeroMaisAleatorioPossivel);
+
+        }
+    }
+
+}
