f5104d874461e6726477aa4aa1e23d34b9fe4ba7
Arend <matheus.arend@univille.br>
2023-01-15 12:31:16 -0300

    Aula 89 - Step 2

--- Capitulo 7/Aula 89 - DateTime/aula89/aula89/Program.cs
+++ Capitulo 7/Aula 89 - DateTime/aula89/aula89/Program.cs
@@ -8,15 +8,36 @@ namespace aula89
 
         static void Main(string[] args)
         {
+            /*
+            DateTime dataTeste = DateTime.Now;
 
-            DateTime d1 = DateTime.Now;
+            Console.WriteLine(dataTeste);
+
+            //Formato de como o pc armazena a data.
+            Console.WriteLine(dataTeste.Ticks);
+            */
+
+            //---------------------------------------------------------
+
+            //Criando um DateTime manualmente.
+            DateTime d1 = new DateTime(2018, 11, 25);
+
+            //Criando um DateTime manualmente com segundos.
+            DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3);
+
+            
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow;
 
             Console.WriteLine(d1);
-            Console.WriteLine(d1.Ticks);
+            Console.WriteLine(d2);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
 
-            long numeroMaisAleatorioPossivel = (d1.Ticks * 3) / 233333333666999;
 
-            Console.WriteLine(numeroMaisAleatorioPossivel);
+
 
         }
     }
