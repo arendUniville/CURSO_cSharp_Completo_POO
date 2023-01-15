b4c007a82abb2f3fd7a88e4e713c63deb571aac0
Arend <matheus.arend@univille.br>
2023-01-15 10:59:12 -0300

    Aula 85 - Step 1

---
+++ Capitulo 7/Aula 85 - Interferencia de tipos - palavra var/aula85/aula85/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace aula85
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var x = 10;
+            var y = 20.0;
+            var z = "30";
+
+
+            Console.WriteLine(x + " - Tipo: " + x.GetType());
+            Console.WriteLine(y.ToString("F1", CultureInfo.InvariantCulture) + " - Tipo: " + y.GetType());
+            Console.WriteLine(z + " - Tipo: " + z.GetType());
+
+        }
+    }
+}
