dbc6f32ae113270e902dc27dfd291a1213bd524f
Arend <matheus.arend@univille.br>
2023-01-15 11:44:10 -0300

    Aula 88 - Step 1

---
+++ Capitulo 7/Aula 88 - Funções para string/aula88/aula88/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace aula88
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string original = "abcde FGHIJ ABC abc DEFG    ";
+        }
+    }
+
+}
