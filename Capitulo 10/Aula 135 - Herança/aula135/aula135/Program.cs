3b42d5993f828a57addf9a55a9360f8277c8ae8b
Arend <matheus.arend@univille.br>
2023-01-22 15:35:08 -0300

    Aula 136 - Step 2

--- Capitulo 10/Aula 135 - Herança/aula135/aula135/Program.cs
+++ Capitulo 10/Aula 135 - Herança/aula135/aula135/Program.cs
@@ -14,10 +14,46 @@ namespace aula135
             BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
 
-            // UPCASTING
+            // UpCasting
 
             Account acc1 = bacc;
             Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+
+            // DownCasting - É uma operação insegura e deve ser usada apenas em casos necessários, depois de ter a validação se o tipo que está vindo realmente é esperado.
+
+                                   // Forçando um tipo.
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+
+
+            // Validando se o tipo vindo é realmente o esperado (Business account).
+            if(acc3 is BusinessAccount) 
+            {
+
+                // Forma comum de Casting:
+                    //BusinessAccount acc5 = (BusinessAccount)acc3;
+
+                // Outra forma de Casting:
+                    BusinessAccount acc5 = acc3 as BusinessAccount;
+
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+
+            }
+
+
+            // Validando se o tipo vindo é realmente o esperado (Savings account).
+            if (acc3 is SavingsAccount)
+            {
+
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+
+                Console.WriteLine("Update!");
+
+
+            }
 
         }
 
