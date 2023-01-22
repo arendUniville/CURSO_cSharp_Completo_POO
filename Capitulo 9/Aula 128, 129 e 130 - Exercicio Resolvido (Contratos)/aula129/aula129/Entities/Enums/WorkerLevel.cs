d34d0af468ef8133638729e14f2babfab10e9b4f
Arend <matheus.arend@univille.br>
2023-01-22 10:34:49 -0300

    Aula 129 - Step 1

---
+++ Capitulo 9/Aula 128, 129 e 130 - Exercicio Resolvido (Contratos)/aula129/aula129/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace aula129.Entities.Enums
+{
+    enum  WorkerLevel : int
+    {
+
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+
+    }
+}
---
+++ Capitulo 9/Aula 128, 129 e 130 - Exercicio Resolvido (Contratos)/aula129/aula129/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace aula129
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+    }
+
+}
