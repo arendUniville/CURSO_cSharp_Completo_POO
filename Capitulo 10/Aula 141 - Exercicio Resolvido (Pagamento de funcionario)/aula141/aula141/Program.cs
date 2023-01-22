69d6658933ebe53b3664d78d70e276b05bb17bb7
Arend <matheus.arend@univille.br>
2023-01-22 19:33:14 -0300

    Aula 141 - Step 2

--- Capitulo 10/Aula 141 - Exercicio Resolvido (Pagamento de funcionario)/aula141/aula141/Program.cs
+++ Capitulo 10/Aula 141 - Exercicio Resolvido (Pagamento de funcionario)/aula141/aula141/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using aula141.Entities;
+using System.Globalization;
 
 namespace aula141
 {
@@ -10,6 +12,58 @@ namespace aula141
         static void Main(string[] args)
         {
 
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());  
+
+
+            for(int i = 1; i <= n; i++) 
+            {
+
+                Console.WriteLine();
+                Console.WriteLine($"Employee {i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch == 'y')
+                {
+
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+
+                }
+                else
+                {
+
+                    list.Add(new Employee(name, hours, valuePerHour));
+
+                }
+
+            
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("\nPayments:\n");
+
+            foreach(Employee emp in list)
+            {
+
+                Console.WriteLine(emp.Name + " - $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+
+            }
 
 
         }
