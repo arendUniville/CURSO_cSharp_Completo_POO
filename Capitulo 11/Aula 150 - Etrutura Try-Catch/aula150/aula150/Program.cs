505e36965954973a37d1efcf7974416c9af69e3e
Arend <matheus.arend@univille.br>
2023-01-30 15:03:47 -0300

    Aula 150 - Step 1

---
+++ Capitulo 11/Aula 150 - Etrutura Try-Catch/aula150/aula150/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace aula150
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+
+            }catch(DivideByZeroException e)
+            {
+
+                Console.WriteLine("Error!" + e.Message);
+
+            }catch(FormatException u) {
+            
+                Console.WriteLine("Format error! " + u.Message);
+            }
+
+            
+
+        }
+
+    }
+
+}
