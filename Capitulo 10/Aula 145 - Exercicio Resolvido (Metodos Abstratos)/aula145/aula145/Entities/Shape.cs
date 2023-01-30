ad5970aa421bd879dc41738679dda064747ef992
Arend <matheus.arend@univille.br>
2023-01-30 12:43:45 -0300

    Aula 145 - Step 1

---
+++ Capitulo 10/Aula 145 - Exercicio Resolvido (Metodos Abstratos)/aula145/aula145/Entities/Shape.cs
@@ -0,0 +1,20 @@
+﻿using aula145.Entities.Enums;
+
+namespace aula145.Entities
+{
+    abstract class Shape
+    {
+
+        public Color Color{ get; set; }
+
+        public Shape(Color color)
+        {
+
+            Color = color;
+
+        }
+
+        public abstract double Area();
+
+    }
+}
---
+++ Capitulo 10/Aula 145 - Exercicio Resolvido (Metodos Abstratos)/aula145/aula145/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace aula145
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+    }
+
+}
