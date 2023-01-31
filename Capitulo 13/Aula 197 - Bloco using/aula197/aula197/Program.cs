6237d516b5a8347a8639fb3f025c511293c47da6
Arend <matheus.arend@univille.br>
2023-01-31 16:53:34 -0300

    Aula 197 - Step 1

---
+++ Capitulo 13/Aula 197 - Bloco using/aula197/aula197/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+
+namespace aula197
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Temp\file1.txt";
+
+            //Method one
+            try
+            {
+                /*
+                
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+
+                        while (!sr.EndOfStream)
+                        {
+
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+
+                    }
+
+                }
+
+                */
+
+            }
+            catch (IOException e)
+            {
+                /*
+                Console.WriteLine(e.Message);
+                */
+            }
+
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+
+                    }
+
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+
+    }
+
+}
