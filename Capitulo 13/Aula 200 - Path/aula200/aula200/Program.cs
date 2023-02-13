9671305f225168696c173169f1543f57ee606bea
Arend <matheus.arend@univille.br>
2023-02-13 10:46:24 -0300

    Aula 200 - Finished

---
+++ Capitulo 13/Aula 200 - Path/aula200/aula200/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace aula200
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Temp\myFolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+
+        }
+
+    }
+
+}
