c087b7989d9541198681599cacab397de3b14575
Arend <matheus.arend@univille.br>
2023-02-13 10:40:21 -0300

    Aula 199 - Finisehd

--- Capitulo 13/Aula 199 - Directory e DirectoryInfo/aula199/aula199/Program.cs
+++ Capitulo 13/Aula 199 - Directory e DirectoryInfo/aula199/aula199/Program.cs
@@ -16,23 +16,50 @@ namespace aula199
 
             try
             {
-                
+
+
+                /*
                 // FORMA 1 
                 IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
 
                 // FORMA 2 
                 //var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
 
+                */
+
+
+                //--------------------------Folders
+
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
                 Console.WriteLine("Folders:");
 
-                foreach (string s in folders) 
+                foreach (string s in folders)
                 {
 
                     Console.WriteLine(s);
 
                 }
 
-            }catch(IOException e) 
+                //--------------------------Files
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+
+                Console.WriteLine("Files:");
+
+                foreach (string s in files)
+                {
+
+                    Console.WriteLine(s);
+
+                }
+
+
+                Directory.CreateDirectory(path + "\\newFolder");
+
+
+            }
+            catch(IOException e) 
             {
 
                 Console.WriteLine("An error occurred");
