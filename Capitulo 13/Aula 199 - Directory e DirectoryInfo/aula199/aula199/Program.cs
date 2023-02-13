8009ddd566d30d103f32c33fb188dcc0c0ed9d65
Arend <matheus.arend@univille.br>
2023-02-13 10:34:32 -0300

    Aula 199 - Step 2

--- Capitulo 13/Aula 199 - Directory e DirectoryInfo/aula199/aula199/Program.cs
+++ Capitulo 13/Aula 199 - Directory e DirectoryInfo/aula199/aula199/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 
 namespace aula199
@@ -9,8 +11,30 @@ namespace aula199
 
         static void Main(string[] args) 
         {
-        
-            
+
+            string path = @"C:\Temp\myFolder";
+
+            try
+            {
+
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+                Console.WriteLine("Folders:");
+
+                foreach (string s in folders) 
+                {
+
+                    Console.WriteLine(s);
+
+                }
+
+            }catch(IOException e) 
+            {
+
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+
+            }
 
         }
 
