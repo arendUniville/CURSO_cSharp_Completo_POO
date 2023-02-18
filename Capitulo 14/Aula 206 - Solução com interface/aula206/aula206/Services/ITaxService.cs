05917357096e9a662bfc9458f848847461284a72
Arend <matheus.arend@univille.br>
2023-02-18 11:38:47 -0300

    Aula 206 - Finish

---
+++ Capitulo 14/Aula 206 - Solução com interface/aula206/aula206/Services/ITaxService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula206.Services
+{
+    interface ITaxService
+    {
+
+        double Tax(double amount);
+
+
+
+    }
+}
