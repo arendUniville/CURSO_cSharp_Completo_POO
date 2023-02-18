0be0fcbd8401303a6e966cb153d5055e262ed64d
Arend <matheus.arend@univille.br>
2023-02-18 10:45:36 -0300

    Init

---
+++ Capitulo 14/Aula 204 e 205 - Solução sem interface/aula204e205/aula204e205/Entities/Invoice.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace aula204e205.Entities
+{
+    internal class Invoice
+    {
+
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+
+            BasicPayment = basicPayment;
+            Tax = tax;
+
+        }
+
+
+        public double TotalPayment { 
+        
+            get { return BasicPayment + Tax; } 
+        
+        }
+
+    }
+}
