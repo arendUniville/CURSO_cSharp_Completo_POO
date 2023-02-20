cd02d37996c6b6fa3d139039a45c9c4725d115b8
Arend <matheus.arend@univille.br>
2023-02-20 12:56:30 -0300

    Aula 213 - Step 1

---
+++ Capitulo 15/Aula 213 e 214 - Generics Pt1 e Pt2/aula213e214/aula213e214/PrintService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace aula213e214
+{
+    class PrintService
+    {
+
+        private int[] _values = new int[10];
+
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+
+            if(_count == 9)
+            {
+
+                throw new InvalidOperationException("PrintService is full.");
+
+            }
+
+            _values[_count] = value;
+            _count++;
+
+        }
+
+
+        public int First()
+        {
+
+            if (_count == 0)
+            {
+
+                throw new InvalidOperationException("PrintService is empty.");
+
+            }
+
+            return _values[0];
+
+        }
+
+        public void Print()
+        {
+
+            Console.WriteLine("[");
+
+            for(int i = 0; i < _count - 1; i++)
+            {
+
+                Console.WriteLine(_values[i] + ", ");
+
+            }
+
+            if(_count > 0)
+            {
+
+                Console.WriteLine(_values[_count - 1]);
+
+            }
+
+            Console.WriteLine("]");
+
+
+        }
+
+    }
+
+}
---
+++ Capitulo 15/Aula 213 e 214 - Generics Pt1 e Pt2/aula213e214/aula213e214/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace aula213e214
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            PrintService printService = new PrintService();
+
+            
+
+        }
+
+    }
+
+}
