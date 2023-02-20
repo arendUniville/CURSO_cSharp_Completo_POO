ff9eba6b684a7d23fb7c8336d26a1e17feaf8a86
Arend <matheus.arend@univille.br>
2023-02-20 16:21:03 -0300

    Aula 215 - Finish

---
+++ Capitulo 15/Aula 215 - Restrição de generics/aula215/aula215/Entities/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula215.Entities
+{
+    class Product : IComparable 
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+        public Product(string name, double price)
+        {
+
+            Name = name;
+            Price = price;
+
+        }
+
+        public override string ToString()
+        {
+            return Name
+                    + ", "
+                    + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+
+                throw new ArgumentException("Comparing error: argument is not a product.");
+
+            }
+
+            Product other = obj as Product;
+
+            return Price.CompareTo(other.Price);
+
+        }
+    }
+}
