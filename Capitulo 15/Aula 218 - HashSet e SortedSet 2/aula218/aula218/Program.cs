e5d61d98345615753e3d3cd1e97ee79ba3dd2fe2
Arend <matheus.arend@univille.br>
2023-02-22 12:47:14 -0300

    Aula 218 - Step 1

---
+++ Capitulo 15/Aula 218 - HashSet e SortedSet 2/aula218/aula218/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula218
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+
+            PrintCollection(a);
+
+        }
+
+
+        static void PrintCollection<T>(IEnumerable<T> collection) 
+        {
+
+            foreach (T obj in collection) 
+            {
+            
+                Console.Write(obj + " ");
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+    }
+
+}
