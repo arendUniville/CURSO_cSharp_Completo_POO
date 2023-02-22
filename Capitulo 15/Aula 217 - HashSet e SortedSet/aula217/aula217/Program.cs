f9b5dc08a0b8e80e1b00b12ac090880ff525fadf
Arend <matheus.arend@univille.br>
2023-02-22 12:42:17 -0300

    Aula 217 - Step 1

---
+++ Capitulo 15/Aula 217 - HashSet e SortedSet/aula217/aula217/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula217
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+
+            Console.WriteLine(set.Contains("Caixa"));
+
+            foreach(string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+
+        }
+    }
+
+}
