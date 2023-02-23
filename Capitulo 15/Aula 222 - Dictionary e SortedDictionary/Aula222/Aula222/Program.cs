ac0e7b6af2df7c837f9c1bf6008dd8b6a4ad04b0
Arend <matheus.arend@univille.br>
2023-02-23 18:15:37 -0300

    Aula 222 - Step 1

---
+++ Capitulo 15/Aula 222 - Dictionary e SortedDictionary/Aula222/Aula222/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula222
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+
+            //Iniciou valendo este valor:
+            cookies["phone"] = "0912302109";
+
+            //Sobrescreveu o valor:
+            cookies["phone"] = "2093203801";
+
+        }
+
+    }
+
+}
