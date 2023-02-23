36eaa8c8e26a8a75114fa781a58e64713a3b0097
Arend <matheus.arend@univille.br>
2023-02-23 18:19:11 -0300

    Aula 222 - Finished

--- Capitulo 15/Aula 222 - Dictionary e SortedDictionary/Aula222/Aula222/Program.cs
+++ Capitulo 15/Aula 222 - Dictionary e SortedDictionary/Aula222/Aula222/Program.cs
@@ -20,6 +20,35 @@ namespace Aula222
             //Sobrescreveu o valor:
             cookies["phone"] = "2093203801";
 
+            cookies.Remove("email");
+
+
+            if (cookies.ContainsKey("email"))
+            {
+
+                Console.WriteLine(cookies["email"]);
+
+            }
+            else
+            {
+
+                Console.WriteLine("There is no 'email' key");
+            
+            }
+
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+
+
+            Console.WriteLine("All cookies: ");
+            foreach(KeyValuePair<string, string> item in cookies)
+            {
+
+                Console.WriteLine(item.Key + ": " + item.Value);
+
+            }
+
         }
 
     }
