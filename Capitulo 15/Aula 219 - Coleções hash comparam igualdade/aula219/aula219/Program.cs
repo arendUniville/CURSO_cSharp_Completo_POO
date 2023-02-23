ac994e9e70169372f696f6121960680e80deba8b
Arend <matheus.arend@univille.br>
2023-02-23 12:52:28 -0300

    Aula 219 - Step 1

---
+++ Capitulo 15/Aula 219 - Coleções hash comparam igualdade/aula219/aula219/Program.cs
@@ -0,0 +1,31 @@
+﻿using aula219.Entities;
+using System;
+
+
+namespace aula219
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            HashSet<Product> a = new HashSet<Product>();
+
+            a.Add(new Product("TV", 900.0));
+            a.Add(new Product("Notebook", 1.200));
+
+
+            HashSet<Point> b = new HashSet<Point>();
+
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1.200);
+
+        }
+
+    }
+
+}
