2259b6f6eca83f89fb8d0157a27bdd333ee1cd69
Arend <matheus.arend@univille.br>
2023-02-23 17:02:44 -0300

    Aula 220 - Step 1

---
+++ Capitulo 15/Aula 220 - Exercicio resolvido sobre conjuntos/aula220/aula220/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace aula220
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+
+                    }
+
+                }
+
+            }catch(IOException e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+    }
+
+}
