e195ee40978be538a007082ffe6bbeefe34475ec
Arend <matheus.arend@univille.br>
2023-02-25 09:31:34 -0300

    Aula 226 - Step 1

---
+++ Capitulo 16/aula226/aula226/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using aula226.Extensions;
+
+namespace aula226
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2018, 11, 16, 6, 10, 45);
+
+            Console.WriteLine(dt.ElapsedTime());
+
+        }
+
+    }
+
+}
