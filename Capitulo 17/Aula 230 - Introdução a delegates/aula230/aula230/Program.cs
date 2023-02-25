221c9463023162e175508a290f49e7124b937299
Arend <matheus.arend@univille.br>
2023-02-25 11:48:07 -0300

    Aula 230 - Finished

---
+++ Capitulo 17/Aula 230 - Introdução a delegates/aula230/aula230/Program.cs
@@ -0,0 +1,39 @@
+﻿using aula230.Services;
+using System;
+
+
+namespace aul230
+{
+
+    class Program
+    {
+
+
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+
+            double a = 10;
+            double b = 12;
+
+            //Forma 1
+            BinaryNumericOperation op = CalculationService.Sum;
+            
+            double result = op(a, b);
+            
+            Console.WriteLine(result);
+
+            
+            //Forma 2
+            BinaryNumericOperation opp = new BinaryNumericOperation(CalculationService.Sum);
+            
+            double resultt = opp(a, b);
+
+            Console.WriteLine(resultt);
+            
+        }
+
+    }
+
+}
