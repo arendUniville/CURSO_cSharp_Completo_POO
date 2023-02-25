be4a0fa929778b29e84080671137909b430f076e
Arend <matheus.arend@univille.br>
2023-02-25 11:57:01 -0300

    Aula 231 - Step 1

---
+++ Capitulo 17/Aula 231 - Multicast Delegates/aula231/aula231/Services/CalculationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula231.Services
+{
+    class CalculationService
+    {
+
+        public static double ShowMax(double x, double y) { return (x > y) ? x : y; }
+
+        public static double ShowSum(double x, double y) {  return x * y; }
+
+        public static double Square(double x) { return x * x; }
+
+    }
+}
