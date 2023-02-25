28fece7806947715b8f14d754c1b6407d66795f8
Arend <matheus.arend@univille.br>
2023-02-25 12:16:27 -0300

    Aula 233 - Finished

--- Capitulo 17/Aula 233 - Action (Exemplo ForEach)/aula233/aula233/Program.cs
+++ Capitulo 17/Aula 233 - Action (Exemplo ForEach)/aula233/aula233/Program.cs
@@ -19,6 +19,25 @@ namespace aula233
             list.Add(new Product("HD Case", 80.90));
 
 
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
+
+
+            list.ForEach(act);
+            
+            foreach(Product p in list)
+            {
+
+                Console.WriteLine(p);
+
+            }
+
+        }
+
+
+        static void UpdatePrice(Product p) 
+        {
+
+            p.Price += p.Price * 0.1;
 
         }
 
