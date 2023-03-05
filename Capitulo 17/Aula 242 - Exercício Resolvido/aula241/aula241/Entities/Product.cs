fa9bb401b9cc37194d26a46893b1cd13cff8ac6b
Arend <matheus.arend@univille.br>
2023-03-05 10:30:18 -0300

    Aula 241 - Step 2

--- Capitulo 17/Aula 242 - Exercício Resolvido/aula241/aula241/Entities/Product.cs
+++ Capitulo 17/Aula 242 - Exercício Resolvido/aula241/aula241/Entities/Product.cs
@@ -12,5 +12,10 @@ namespace aula241.Entities
         public string Name { get; set; }
         public double Price { get; set; }
 
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
     }
 }
